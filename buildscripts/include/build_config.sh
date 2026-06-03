e914915347c994c055dae886f64531981c219fb9
SunnyVishnu3 <krishnasgnik3@gmail.com>
2026-06-03 14:31:48 +0530

    disable x86 and 64

--- buildscripts/include/build_config.sh
+++ buildscripts/include/build_config.sh
@@ -1,6 +1,6 @@
 # Build Configuration
 # Set to true to enable x86 and x86_64 Android architecture builds
-ENABLE_X86_ARCH=true
+ENABLE_X86_ARCH=false
 # Set to true to enable ARM v9a optimized build (ships alongside v8a in assets)
 # Devices with SVE2 (Snapdragon 8 Gen 2+, Dimensity 9200+, Exynos 2400+) get 15-18% boost
 ENABLE_ARM_V9A=true
